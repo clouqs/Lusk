1fff17cf2aa2751cac6f38efcf4b370cf4f30104
bulligan <zetrox.gota@gmail.com>
2026-03-05 20:30:22 +0100

    commit

--- server/routes.ts
+++ server/routes.ts
@@ -7,7 +7,7 @@ import { isAuthenticated } from "./replit_integrations/auth/replitAuth";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.AI_INTEGRATIONS_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY ?? process.env.AI_INTEGRATIONS_OPENAI_API_KEY,
   baseURL: process.env.AI_INTEGRATIONS_OPENAI_BASE_URL,
 });
 
